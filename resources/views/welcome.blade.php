e78c313fc8c1ea0e8b5725649a505cabd7bf8066
161710134 <161710134@smkassalaam.sch.id>
2019-07-05 13:56:08 +0700

    wkkw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Candra
                 </div>
 
                 <div class="links">
